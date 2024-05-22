34bfca0bf66be074cec667c64877eacb4de965a9
Christian Solano <christiansolano@Christians-MacBook-Air-2.local>
2024-05-22 11:40:56 -0700

    Added threading

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -17,6 +17,7 @@ SLIST_HEAD(list_head, list_entry);
 
 struct hash_table_entry {
 	struct list_head list_head;
+    pthread_mutex_t lock;
 };
 
 struct hash_table_v2 {
@@ -30,6 +31,10 @@ struct hash_table_v2 *hash_table_v2_create()
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		SLIST_INIT(&entry->list_head);
+        if (pthread_mutex_init(&entry->lock, NULL) != 0) {
+            free(hash_table);
+            return NULL;
+        }
 	}
 	return hash_table;
 }
@@ -73,6 +78,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              uint32_t value)
 {
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
+    pthread_mutex_lock(&hash_table_entry->lock);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
 
@@ -86,6 +92,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+    pthread_mutex_unlock(&hash_table_entry->lock);
 }
 
 uint32_t hash_table_v2_get_value(struct hash_table_v2 *hash_table,
@@ -109,6 +116,7 @@ void hash_table_v2_destroy(struct hash_table_v2 *hash_table)
 			SLIST_REMOVE_HEAD(list_head, pointers);
 			free(list_entry);
 		}
+        pthread_mutex_destroy(&entry->lock);
 	}
 	free(hash_table);
 }
